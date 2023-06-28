7fdab95adf44eca943744ff22f3dcf6056c362ae
Nachos29 <mc29360@gmail.com>
2023-06-28 16:08:36 +0200

    Tout est ok plus que bookings

--- cart.js
+++ cart.js
@@ -2,30 +2,67 @@ fetch('http://localhost:3000/cart')
         .then(response => response.json())
         .then(data => {
             document.querySelector('#my-cart').innerHTML = "";
+            document.querySelector('#my-cart').innerHTML = "<h2>My cart</h2>"
+            
+            let totalPrice = 0
+
             for (let cart of data) {
                 
-                const cartElement = document.createElement('div');
-                cartElement.id = 'cart-datas';
-                cartElement.innerHTML = `
+                document.querySelector('#my-cart').innerHTML += 
+                `<div class="cart-datas">
                 <div id="places">${cart.travelInfo}</div>
                 <div id="time">${cart.departureTime}</div>
                 <div id="price">${cart.price}</div>
                 <button class="delete-button">✖</button>`;
-                document.querySelector('#my-cart').appendChild(cartElement);
+
+                let priceToArray = cart.price.split("")
+                priceToArray.pop()
+                let arrayToNumber = Number(priceToArray.join(""))
+                totalPrice += arrayToNumber
+
             }
 
-            document.querySelectorAll('.delete-button').forEach(button => {
-                button.addEventListener('click', function () {
-                    const departureTime = this.parentNode.querySelector('div:nth-child(2)').textContent; // Récupère la valeur de departureTime à partir du div parent
+            const purchase = document.createElement('div');
+            purchase.id = 'total'
+            purchase.innerHTML += `<div id= "total">
+            <div id="priceTotal"> </div>
+            <button id="purchase-button">Purchase</button>
+            </div>`;
+            document.querySelector('#my-cart').appendChild(purchase)
+            document.querySelector('#priceTotal').textContent = `Total: ${totalPrice} €`
+
+            function updateDeleteTrip() {
+                for (let i = 0; i < document.querySelectorAll('.delete-button').length; i++) {
+                    document.querySelectorAll('.delete-button')[i].addEventListener('click', function () {
+                  const departureTime = this.parentNode.querySelector('div:nth-child(2)').textContent; 
             
-                    fetch(`http://localhost:3000/cart?departureTime=${departureTime}`, {
-                    method: 'DELETE' })
+                    fetch(`http://localhost:3000/cart?departureTime=${departureTime}`, { method: 'DELETE' })
                         .then(response => response.json())
                         .then(data => {
                             if (data) {
                                 this.parentNode.remove();
+                    updateCartTotal()
                             }
-                        })
-                })
-            })
-        })
+                        });
+                });
+            }
+            }
+            updateDeleteTrip()
+            
+            });
+            
+            //Fonction pour mettre à jour le panier total après suppression d'un élément
+            function updateCartTotal() {
+              fetch('http://localhost:3000/cart')
+                .then(response => response.json())
+                .then(data => {
+                  let totalPrice = 0;
+                  for (const key of data) {
+                    let priceToArray = key.price.split("");
+                    priceToArray.pop();
+                    let arrayToNumber = Number(priceToArray.join(""));
+                    totalPrice += arrayToNumber;
+                  }
+                  document.querySelector('#priceTotal').textContent = `Total: ${totalPrice} €`
+                });
+            }
