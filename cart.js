8866feec46785b9b63676a0b10767ce78e03f33e
Ines <ines.oliete@gmail.com>
2023-06-28 12:07:49 +0200

    Cart ok

--- cart.js
+++ cart.js
@@ -1,24 +1,16 @@
-document.querySelector("#myCart").addEventListener('DOMContentLoaded', function() {
-
-    fetch('http://localhost:3000/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(trip),
-    })
+fetch('http://localhost:3000/cart')
         .then(response => response.json())
         .then(data => {
-            document.querySelector('#myCart').innerHTML = "";
+            document.querySelector('#my-cart').innerHTML = "";
             for (let cart of data) {
                 
                 const cartElement = document.createElement('div');
-                //cartElement.id = 'trip-datas';
+                cartElement.id = 'cart-datas';
                 cartElement.innerHTML = `
                 <div id="places">${cart.travelInfo}</div>
                 <div id="time">${cart.departureTime}</div>
                 <div id="price">${cart.price}€</div>
                 <button class="delete">✖</button>`;
-                
-                //document.querySelector('#output-container').appendChild(tripElement);
+                document.querySelector('#my-cart').appendChild(cartElement);
             }
         })  
-});
